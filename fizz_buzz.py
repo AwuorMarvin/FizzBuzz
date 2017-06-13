2a9981d7a4a75d9ac98bbf400669635a2634f8dd
Awuor Marvin <awuor.marvin5@gmail.com>
2017-06-13 10:12:40 -0700

    Refactoring and debugging to ensure it passes all the tests

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,11 +1,11 @@
 def fizz_buzz(num):
-      if num%15 == 0:
-    print('FizzBuzz')
-  elif num%3 == 0:
-    print('Fizz')
-  elif num%5 == 0:
-    print('Buzz')
-  else:
-    print(num)
-    
+    if num%15 == 0:
+        return 'FizzBuzz'
+    elif num%3 == 0:
+        return 'Fizz'
+    elif num%5 == 0:
+        return 'Buzz'
+    else:
+        return num
+        
 fizz_buzz(30)
